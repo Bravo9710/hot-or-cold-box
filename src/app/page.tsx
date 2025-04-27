7240bcc43ca2bb9216d70b54c3bb41e96f3cc13f
Vencislav Venkov <wenci_97_08@abv.bg>
2025-04-28 01:50:42 +0300

    Add finishing details.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import FancyBox from "./components/fancyBox";
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
@@ -21,16 +22,18 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <header className="bg-blue-900 text-white py-8 text-center">
-        <h1 className="text-4xl font-bold">The</h1>
+      <header className="bg-[#021024] text-white pt-[66px] pb-[91px] text-center">
+        <h1 className="text-5xl font-medium tracking-[0.2em] leading-[60px] text-[#36FCF0]">
+          THE
+        </h1>
         <h2 className="text-6xl font-extrabold">Hot or Cold Box</h2>
       </header>
 
-      <div className="bg-blue-100 h-16">
-        {/* Divider SVG will be placed here */}
-      </div>
+      <div
+        className="bg-no-repeat bg-center bg-cover w-100% h-[218px] mt-[-20px]"
+        style={{ backgroundImage: "url('/images/layer.svg')" }}></div>
 
-      <main className="flex-grow flex items-center justify-center">
+      <main className="flex-grow flex items-center justify-center py-[120px]">
         <FancyBox
           isMobile={isMobile}
           srp={96.5}
